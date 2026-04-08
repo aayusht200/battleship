18d41b2ca92266aacc2e98775714221698cdb8db
Aayush Trivedi <aayush.t200@gmail.com>
2026-04-08 13:36:14 +0530

    ship class and test done

--- src/ship.cjs
+++ src/ship.cjs
@@ -1,6 +1,13 @@
 class Ship {
     constructor(length) {
         this.length = length;
+        this.health = 0;
+    }
+    hit() {
+        if (!this.isSunk()) this.health++;
+    }
+    isSunk() {
+        return this.health === this.length;
     }
 }
 
--- tests/ship.test.js
+++ tests/ship.test.js
@@ -4,3 +4,38 @@ it('creates a ship with correct length', () => {
     const ship = new Ship(3);
     expect(ship.length).toBe(3);
 });
+
+it('initial health is zero', () => {
+    const ship = new Ship(3);
+    expect(ship.health).toBe(0);
+});
+
+it('increments health when hit is called', () => {
+    const ship = new Ship(3);
+    ship.hit();
+    expect(ship.health).toBe(1);
+});
+
+it('is not sunk after one hit', () => {
+    const ship = new Ship(3);
+    ship.hit();
+    expect(ship.isSunk()).toBe(false);
+});
+
+it('is sunk when health equals length', () => {
+    const ship = new Ship(3);
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk()).toBe(true);
+});
+
+it('does not increase health after ship is sunk', () => {
+    const ship = new Ship(3);
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(ship.health).toBe(3);
+    expect(ship.isSunk()).toBe(true);
+});
