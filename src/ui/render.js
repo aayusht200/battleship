2af82a961eb19134e7e4bccc63b92878ab542e56
Aayush Trivedi <aayush.t200@gmail.com>
2026-04-20 11:30:49 +0530

    Refactor CSS and UI rendering: enhance layout styles, improve button interactions, and update game board structure for better user experience

--- src/ui/render.js
+++ src/ui/render.js
@@ -6,11 +6,17 @@ export default function render(state) {
     const playerArea = document.createElement('div');
     const computerArea = document.createElement('div');
     const statusArea = document.createElement('div');
+    const playerTag = document.createElement('div');
+    const computerTag = document.createElement('div');
+    playerTag.className = 'tag-label';
+    computerTag.className = 'tag-label';
+    playerTag.innerText = 'Player Board';
+    computerTag.innerText = 'Computer Board';
     playerArea.className = 'player-area';
     computerArea.className = 'computer-area';
     statusArea.className = 'status-area';
-    playerArea.append(renderBoard(state.playerBoard, state.playerAttacked, 'player'));
-    computerArea.append(renderBoard(state.computerBoard, state.computerAttacked, 'computer'));
+    playerArea.append(playerTag, renderBoard(state.playerBoard, state.playerAttacked, 'player'));
+    computerArea.append(computerTag, renderBoard(state.computerBoard, state.computerAttacked, 'computer'));
     statusArea.append(renderStatus(state.state));
     gameContainer.append(playerArea, computerArea, statusArea);
 }
--- src/ui/renderStatus.js
+++ src/ui/renderStatus.js
@@ -1,14 +1,12 @@
 export default function renderStatus(state) {
     const status = document.createElement('div');
     status.className = 'status-text';
-
+    const button = document.createElement('button');
+    button.className = 'reset-btn';
     status.textContent = state.winner === null ? state.status : `${state.winner} wins`;
+    if (state.status === 'ongoing') button.textContent = 'Rearrange Ships';
+    if (state.status === 'over') button.textContent = 'Reset Game';
 
-    if (state.status === 'over') {
-        const button = document.createElement('button');
-        button.className = 'reset-btn';
-        button.textContent = 'Reset Game';
-        status.append(button);
-    }
+    status.append(button);
     return status;
 }
